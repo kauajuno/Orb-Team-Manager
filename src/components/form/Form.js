f147a3fbaae3067911fd44f970de98e8ebdbdca6
Kaua Junio <kauajuno@gmail.com>
2023-05-23 20:27:34 -0300

    New header + dropdown options

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,14 +1,17 @@
 import './Form.css';
 import InputField from '../input_field';
+import Dropdown from '../dropdown';
 
 export const Form = () => {
+    const times = ['DevOps', 'Front-end', 'Back-end', 'Data Science', 'UX e Design', 'Inovação'];
     return (
-        <section>
+        <section className='form'>
             <form>
                 <h2>Preencha os dados do novo integrante</h2>
                 <InputField label="Nome" placeholder="Digite seu nome" />
                 <InputField label="Cargo" placeholder="Informe seu cargo" />
                 <InputField label="Imagem" placeholder="Informe o endereço da imagem" />
+                <Dropdown label="Time" itens={times}/>
             </form>
         </section>
     );
