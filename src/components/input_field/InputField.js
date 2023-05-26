a87578bcaca54899b65a002795e31a14e75d6406
Kaua Junio <kauajuno@gmail.com>
2023-05-26 14:20:55 -0300

    [OrbTM] Changing value input state

--- src/components/input_field/InputField.js
+++ src/components/input_field/InputField.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './InputField.css'
 
 export const InputField = (props) => {
+
+    let [val, setVal] = useState('');
+
+    const onTyping = (e) => {
+        setVal(e.target.value);
+        console.log(val)
+    }
+
     return(
         <div className="input-field">
             <label>{props.label}</label>
-            <input required={props.mandatory} placeholder={props.placeholder}/>
+            <input value={val} onChange={onTyping} required={props.mandatory} placeholder={props.placeholder}/>
         </div>
     )
 }
